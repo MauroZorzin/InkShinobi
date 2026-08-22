3876cb1de79be2db77e9d1620230a991335d213a
Crystal Spider <riccardo.chimisso@gmail.com>
2026-08-22 14:31:02 +0200

    Improve loading and saving between scenes.

--- Assets/Scripts/Rendering/InkTransition.cs
+++ Assets/Scripts/Rendering/InkTransition.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 using UnityEngine.Audio;
 
 public class InkTransition : MonoBehaviour {
+  private const float MaximumAnimationDelta = 1f / 30f;
+
   [Header("Visual")]
   [SerializeField] private Material inkMaterial;
   [SerializeField] private float duration = 1f;
 
   [Header("Audio")]
-  [SerializeField] private AudioClip transitionSound;
+  [SerializeField] private AudioClip fadeInSound;
+  [SerializeField] private AudioClip fadeOutSound;
   [SerializeField] private AudioMixerGroup mixerGroup;
   [Min(0f)]
   [SerializeField] private float revealSoundStartOffset = 0.12f;
@@ -22,21 +25,15 @@ public class InkTransition : MonoBehaviour {
 
   private static readonly int FineDistortion = Shader.PropertyToID("_Fine_Distortion");
   private SceneTransitionManager _owner;
-  private AudioClip _reversedTransitionSound;
 
   private void Awake() {
     _owner = GetComponent<SceneTransitionManager>();
     if (_owner != null && SceneTransitionManager.Instance != _owner) return;
 
     inkMaterial.SetFloat(InkAmount, 0f);
-    if (transitionSound != null) transitionSound.LoadAudioData();
-  }
-
-  private IEnumerator Start() {
-    if (transitionSound == null) yield break;
-
-    while (transitionSound.loadState == AudioDataLoadState.Loading) yield return null;
-    CreateReversedTransitionSound();
+    inkMaterial.SetPass(0);
+    if (fadeInSound != null) fadeInSound.LoadAudioData();
+    if (fadeOutSound != null) fadeOutSound.LoadAudioData();
   }
 
   public IEnumerator CoverScreen() {
@@ -48,56 +45,17 @@ public class InkTransition : MonoBehaviour {
   }
 
   private IEnumerator ApplyToScreen(bool cover = true) {
-    AudioClip sound = cover ? transitionSound : GetReversedTransitionSound();
+    AudioClip sound = cover ? fadeInSound : fadeOutSound;
     SceneTransitionManager.PlayUiSound(sound, mixerGroup, cover ? 0f : revealSoundStartOffset);
     if (cover) RandomizeInk();
     float time = 0f;
     while (time < duration) {
-      time += Time.unscaledDeltaTime;
+      time += Mathf.Min(Time.unscaledDeltaTime, MaximumAnimationDelta);
       float t = Mathf.Clamp01(time / duration);
       inkMaterial.SetFloat(InkAmount, cover ? t : 1f - t);
       yield return null;
     }
     inkMaterial.SetFloat(InkAmount, cover ? 1f : 0f);
-  }
-
-  private AudioClip GetReversedTransitionSound() {
-    if (_reversedTransitionSound == null) CreateReversedTransitionSound();
-    return _reversedTransitionSound != null ? _reversedTransitionSound : transitionSound;
-  }
-
-  private void CreateReversedTransitionSound() {
-    if (_reversedTransitionSound != null || transitionSound == null) return;
-    if (transitionSound.loadState != AudioDataLoadState.Loaded) return;
-
-    int channels = transitionSound.channels;
-    int frameCount = transitionSound.samples;
-    var samples = new float[frameCount * channels];
-    if (!transitionSound.GetData(samples, 0)) return;
-
-    for (int firstFrame = 0, lastFrame = frameCount - 1;
-         firstFrame < lastFrame;
-         firstFrame++, lastFrame--) {
-      int firstSample = firstFrame * channels;
-      int lastSample = lastFrame * channels;
-      for (int channel = 0; channel < channels; channel++) {
-        (samples[firstSample + channel], samples[lastSample + channel]) =
-          (samples[lastSample + channel], samples[firstSample + channel]);
-      }
-    }
-
-    _reversedTransitionSound = AudioClip.Create(
-      $"{transitionSound.name} (Reversed)",
-      frameCount,
-      channels,
-      transitionSound.frequency,
-      false
-    );
-    _reversedTransitionSound.hideFlags = HideFlags.DontSave;
-    if (!_reversedTransitionSound.SetData(samples, 0)) {
-      Destroy(_reversedTransitionSound);
-      _reversedTransitionSound = null;
-    }
   }
 
   private void RandomizeInk() {
@@ -108,9 +66,5 @@ public class InkTransition : MonoBehaviour {
     inkMaterial.SetFloat(FineNoiseScale, Random.Range(22, 28));
     inkMaterial.SetFloat(CoarseDistortion, Random.Range(0.12f, 0.18f));
     inkMaterial.SetFloat(FineDistortion, Random.Range(0.025f, 0.05f));
-  }
-
-  private void OnDestroy() {
-    if (_reversedTransitionSound != null) Destroy(_reversedTransitionSound);
   }
 }
